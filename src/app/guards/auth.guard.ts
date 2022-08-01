c89ed33cb87beadf74f0eb35985383c8749600d9
Srujana <srujana@i-stellar.in>
2022-08-01 13:18:21 +0530

    build

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  Router,
-} from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { RestApiService } from '../services/rest-api.service';
 
 @Injectable({
@@ -13,7 +10,8 @@ export class AuthGuard implements CanActivate {
   canActivate(): boolean {
     const token = localStorage.getItem('token');
 
-    const isLoggedIn = token && token != null && token.length > 0 ? true : false;
+    const isLoggedIn =
+      token && token != null && token.length > 0 ? true : false;
     if (isLoggedIn) {
       return true;
     }
--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -3,14 +3,15 @@ import { CanActivate, Router } from '@angular/router';
 import { RestApiService } from '../services/rest-api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginGuard implements CanActivate {
   constructor(public router: Router, public apiService: RestApiService) {}
   canActivate(): boolean {
     const token = localStorage.getItem('token');
 
-    const isLoggedIn = token && token != null && token.length > 0 ? true : false;
+    const isLoggedIn =
+      token && token != null && token.length > 0 ? true : false;
     if (isLoggedIn) {
       this.router.navigate(['/']);
       return false;
